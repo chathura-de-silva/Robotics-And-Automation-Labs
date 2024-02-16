04bc6d7eaca63cad9aa375d47b9f2a37e2c1770c
chathura-de-silva <ac.chathura@gmail.com>
2024-02-16 14:14:17 +0530

    Lab 4 'Time travel' completed

---
+++ lab4/timeTravel/part2/build/broadcaster_listener/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/chathura/Documents/Git Projects/Robotics-and-Automation-Labs/lab4/timeTravel/part2/install/broadcaster_listener'

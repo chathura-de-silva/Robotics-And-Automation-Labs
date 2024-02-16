4081f4d38b52b89bfc83cd837ff4ef53dcd23c79
chathura-de-silva <ac.chathura@gmail.com>
2024-02-16 16:37:24 +0530

    Lab 4 completely working

---
+++ lab4/Cartographer/src/broadcaster_listener/build/broadcaster_listener/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/chathura/Documents/Git Projects/Robotics-and-Automation-Labs/lab4/Cartographer/src/broadcaster_listener/install/broadcaster_listener'

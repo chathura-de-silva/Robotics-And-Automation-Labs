836a74512deb95c560bc7084c8f2f40a24f9ca90
chathura-de-silva <ac.chathura@gmail.com>
2024-02-16 16:40:22 +0530

    Cleared clutter

--- lab4/Cartographer/src/broadcaster_listener/build/broadcaster_listener/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/chathura/Documents/Git Projects/Robotics-and-Automation-Labs/lab4/Cartographer/src/broadcaster_listener/install/broadcaster_listener'
